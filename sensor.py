912e75bbb84f3dc886237de0762916c13f1a6bbc
kelvin-de-oliveira <kelvin.oliveira.0110@gmail.com>
2026-05-17 23:16:40 -0300

    better handling of keyboard interruptions

--- sensor.py
+++ sensor.py
@@ -10,10 +10,15 @@ producer = KafkaProducer(
 )
 
 temperature = 25.0
-while True:
-    variation = random.uniform(-2.0, 2.0)
-    temperature = round(temperature + variation, 2)
-    event = {'timestamp': time.strftime('%Y-%m-%dT%H:%M:%S'), 'value': temperature}
-    producer.send('topic_sensor', value=event)
-    print(f'Published: {event}')
-    time.sleep(2)
+
+print('Sensor started. Publishing to topic_sensor...')
+try:
+    while True:
+        variation = random.uniform(-2.0, 2.0)
+        temperature = round(temperature + variation, 2)
+        event = {'timestamp': time.strftime('%Y-%m-%dT%H:%M:%S'), 'value': temperature}
+        producer.send('topic_sensor', value=event)
+        print(f'Published: {event}')
+        time.sleep(2)
+except KeyboardInterrupt:
+    print('Sensor stopped.')
